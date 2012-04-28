5fbeb1edbd07db9a8a1fd6cacad4d25b02694521
nick_egboh <nick.egboh@gmail.com>
2012-04-28 17:38:23 -0500

    Just added the definitions of the methods to FlatFS.java and PTree.java

--- FlatFS.java
+++ FlatFS.java
@@ -17,60 +17,103 @@ public class FlatFS{
   public static final int ASK_FREE_FILES = 29545;
   public static final int ASK_FILE_METADATA_SIZE = 3502;
 
+  /* This function is the constructor. If doFormat == false, data 
+   * stored in previous sessions must remain stored. If doFormat == true, 
+   * the system should initialize the underlying disk to empty. 
+   */
   public FlatFS(boolean doFormat)
     throws IOException
   {
   }
 
+  /* This function begins a new transaction and returns an 
+   * identifying transaction ID.
+   */
   public TransID beginTrans()
   {
     return null;
   }
 
+  /* This function commits the specified transaction. 
+   */
   public void commitTrans(TransID xid)
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function aborts the specified transaction.  
+   */
   public void abortTrans(TransID xid)
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function creates a new file and returns the 
+   * inode number (a unique identifier for the file) 
+   */
   public int createFile(TransID xid)
     throws IOException, IllegalArgumentException
   {
     return -1;
   }
 
+  /* This function removes the file specified by the inode 
+   * number inumber. The file is deleted and the 
+   * corresponding resources are reclaimed. 
+   */
   public void deleteFile(TransID xid, int inumber)
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function reads count bytes from the file specified by inumber  into the 
+   * buffer specified by buffer. The parameter offset specifies the starting location 
+   * within the file where the data should be read. Upon success, the function returns 
+   * the number of bytes read (this number can be less than count if offset + count 
+   * exceeds the length of the file. The method throws EOFException if offset is past 
+   * the end of the file.
+   */
   public int read(TransID xid, int inumber, int offset, int count, byte buffer[])
     throws IOException, IllegalArgumentException, EOFException
   {
     return -1;
   }
     
-
+  /* This function writes count bytes from the buffer specified by buffer into the file 
+   * specified by inumber. The parameter offset specifies the starting location within 
+   * the file where the data should be written. Attempting to write beyond the end of 
+   * file should extend the size of the file to accommodate the new data. 
+   */
   public void write(TransID xid, int inumber, int offset, int count, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
-
+  
+  /* This function reads getParam(ASK_FILE_METADATA_SIZE) bytes of per-file metadata 
+   * for tree tnum and stores this data in the buffer beginning at buffer. This per-file 
+   * metadata is an uninterpreted array of bytes that higher-level code may use to store 
+   * state associated with a given file. 
+   */
   public void readFileMetadata(TransID xid, int inumber, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
 
-
+  /* This function writes getParam(ASK_FILE_METADATA_SIZE) bytes of per-file metadata 
+   * for file inumber from the buffer beginning at buffer. 
+   */
   public void writeFileMetadata(TransID xid, int inumber, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function allows applications to get parameters of the file system. The parameter 
+   * is one of FlatFS.ASK_MAX_FILE (to ask the maximum number of files the formatted file 
+   * system supports), FlatFS.ASK_FREE_SPACE_BLOCKS (to ask how many free blocks the file 
+   * system currently has), FlatFS.ASK_FREE_FILES (to ask how many free inodes the system 
+   * currently has), and FlatFS.ASK_FILE_METADATA_SIZE (to ask how much space there is for 
+   * per-file metadata).  It returns an integer answer to the question
+   */
   public int getParam(int param)
     throws IOException, IllegalArgumentException
   {
--- PTree.java
+++ PTree.java
@@ -30,65 +30,115 @@ public class PTree{
   public static final int BLOCK_SIZE_BYTES = 1024;
   public static final int POINTERS_PER_INTERNAL_NODE = 256;
 
-
+  /* This function is the constructor. If doFormat == false, data stored 
+   * in previous sessions must remain stored. If doFormat == true, the 
+   * system should initialize the underlying disk to empty. 
+   */
   public PTree(boolean doFormat)
   {
   }
 
+  /* This function begins a new transaction and returns 
+   * an identifying transaction ID.
+   */
   public TransID beginTrans()
   {
     return null;
   }
 
+  /*
+   * This function commits the specified transaction.
+   */
   public void commitTrans(TransID xid) 
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* 
+   * This function aborts the specified transaction.  
+   */
   public void abortTrans(TransID xid) 
     throws IOException, IllegalArgumentException
   {
   }
 
-
+  /* This function creates a new tree and returns the 
+   * TNum number (a unique identifier for the tree). 
+   */
   public int createTree(TransID xid) 
     throws IOException, IllegalArgumentException, ResourceException
   {
     return -1;
   }
 
+  /* This function removes the tree specified by the tree  
+   * number tnum. The tree is deleted and the corresponding 
+   * resources are reclaimed.
+   */
   public void deleteTree(TransID xid, int tnum) 
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function returns the maximum ID of any data block  
+   * stored in the specified tree. Note that blocks in a 
+   * tree are numbered starting from 0.
+   */
   public void getMaxDataBlockId(TransID xid, int tnum)
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function reads PTree.BLOCK_SIZE_BYTES bytes from the blockId'th 
+   * block of data in the tree specified by tnum into the buffer specified 
+   * by buffer.  If the specified block does not exist in the tree, the 
+   * function should fill *buffer with '\0' values.
+   */
   public void readData(TransID xid, int tnum, int blockId, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
 
-
+  /* This function writes PTREE.BLOCK_SIZE_BYTES bytes from the buffer specified 
+   * by buffer into the blockId'th block of data in the tree specified by tnum. 
+   * If the specified block does not exist in the tree, the function should grow 
+   * the tree to include the new block. Notice that this growth may require 
+   * updating multiple data structures -- the free list, the pointer to the tree 
+   * root, internal tree nodes, and the data block itself -- and all of these 
+   * updates must be done atomically within the transaction. 
+   */
   public void writeData(TransID xid, int tnum, int blockId, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
 
+  /* This function reads PTree.METADATA_SIZE bytes of per-tree metadata for tree tnum 
+   * and stores this data in the buffer beginning at buffer. This per-tree metadata 
+   * is an uninterpreted array of bytes that higher-level code may use to store state 
+   * associated with a given tree. 
+   */
   public void readTreeMetadata(TransID xid, int tnum, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
 
 
+  /* This function writes PTree.METADATA_SIZE bytes of per-tree metadata for 
+   * tree tnum from the buffer beginning at buffer.
+   */
   public void writeTreeMetadata(TransID xid, int tnum, byte buffer[])
     throws IOException, IllegalArgumentException
   {
   }
-
+  
+  /* This function allows applications to get parameters of the persistent tree 
+   * system. The parameter is one of PTree.ASK_FREE_SPACE (to ask how much free 
+   * space the system currently has), PTree.ASK_MAX_TREES (to ask what is the 
+   * maximum number of trees the system can support), and PTree.ASK_FREE_TREES 
+   * (to ask how many free tree IDs the system currently has). It returns an 
+   * integer answer to the question or throws IllegalArgumentException if param 
+   * does not correspond to one of these value.. 
+   */
   public int getParam(int param)
     throws IOException, IllegalArgumentException
   {
